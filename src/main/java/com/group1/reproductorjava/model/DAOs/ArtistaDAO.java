15c22fcfd685d8a4d6f9ee371ae5bdaefea901f9
Jose Javier Galan <josejaviergalan2003@gmail.com>
2023-11-05 09:02:59 +0100

    Clase Artista Completa-Starting ArtistDAO

--- src/main/java/com/group1/reproductorjava/model/DAOs/ArtistaDAO.java
+++ src/main/java/com/group1/reproductorjava/model/DAOs/ArtistaDAO.java
@@ -1,4 +1,36 @@
 package com.group1.reproductorjava.model.DAOs;
 
-public class ArtistaDAO {
+import com.group1.reproductorjava.model.Entity.Artista;
+import com.group1.reproductorjava.model.interfaces.IArtistaDAO;
+
+import java.util.List;
+
+public class ArtistaDAO implements IArtistaDAO {
+
+
+
+    @Override
+    public boolean getArtista(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean getArtista(String name) {
+        return false;
+    }
+
+    @Override
+    public List<Artista> getAllArtists() {
+        return null;
+    }
+
+    @Override
+    public boolean saveArtista(Artista artista) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteArtista(Artista artista) {
+        return false;
+    }
 }

01962e3cacbbbcfe5202df3d5df7afbd59b7e4d8
Jose Javier Galan <josejaviergalan2003@gmail.com>
2023-11-05 09:00:36 +0100

    Clase Artista Completa

--- src/main/java/com/group1/reproductorjava/model/Entity/Artista.java
+++ src/main/java/com/group1/reproductorjava/model/Entity/Artista.java
@@ -1,6 +1,7 @@
 package com.group1.reproductorjava.model.Entity;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Artista {
     int id;
@@ -8,4 +9,76 @@ public class Artista {
     String photo;
     String nacionality;
     List<Disco> discos;
+
+    public Artista(int id, String name, String photo, String nacionality, List<Disco> discos) {
+        this.id = id;
+        this.name = name;
+        this.photo = photo;
+        this.nacionality = nacionality;
+        this.discos = discos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getNacionality() {
+        return nacionality;
+    }
+
+    public void setNacionality(String nacionality) {
+        this.nacionality = nacionality;
+    }
+
+    public List<Disco> getDiscos() {
+        return discos;
+    }
+
+    public void setDiscos(List<Disco> discos) {
+        this.discos = discos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Artista artista = (Artista) o;
+        return id == artista.id && Objects.equals(name, artista.name) && Objects.equals(photo, artista.photo) && Objects.equals(nacionality, artista.nacionality) && Objects.equals(discos, artista.discos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, photo, nacionality, discos);
+    }
+
+    @Override
+    public String toString() {
+        return "Artista{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", photo='" + photo + '\'' +
+                ", nacionality='" + nacionality + '\'' +
+                ", discos=" + discos +
+                '}';
+    }
 }

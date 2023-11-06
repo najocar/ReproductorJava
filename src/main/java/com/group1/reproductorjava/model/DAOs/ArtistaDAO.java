350b95cef4bcaff17bb91d08b5c5a9d5ebe33032
Jose Javier Galan <josejaviergalan2003@gmail.com>
2023-11-06 14:14:00 +0100

    Clase Artista Completa-ArtistDAO Completo

--- src/main/java/com/group1/reproductorjava/model/DAOs/ArtistaDAO.java
+++ src/main/java/com/group1/reproductorjava/model/DAOs/ArtistaDAO.java
@@ -1,36 +1,163 @@
 package com.group1.reproductorjava.model.DAOs;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.group1.reproductorjava.model.Connection.MariaDBConnection;
 import com.group1.reproductorjava.model.Entity.Artista;
 import com.group1.reproductorjava.model.interfaces.IArtistaDAO;
 
-import java.util.List;
-
 public class ArtistaDAO implements IArtistaDAO {
 
+    private static ArtistaDAO instance;
+
+    private final static String INSERT = "INSERT INTO Artista (id, nombre, nacionalidad,foto) VALUES(?, ?, ?, ?)";
+    private final static String UPDATE = "UPDATE Artista SET nombre=?, foto=?, nacionalidad=? WHERE id=?";
+    private final static String DELETE = "DELETE FROM Artista WHERE id=?";
+    private final static String SELECT_BY_ID = "SELECT id, nombre, nacionalidad, foto FROM Artista WHERE id=?";
+    private final static String SELECT_ALL = "SELECT id, nombre,nacionalidad,foto FROM Artista";
+    private final static String SELECT_BY_NAME = "SELECT id, nombre, nacionalidad, foto FROM Artista WHERE nombre=?";
 
 
-    @Override
+    private ArtistaDAO() {
+        // Constructor privado para evitar la creación de instancias directas.
+    }
+
+    public static ArtistaDAO getInstance() {
+        if (instance == null) {
+            instance = new ArtistaDAO();
+        }
+        return instance;
+    }
+
+    public boolean saveArtista(Artista artista) {
+        Connection conn = MariaDBConnection.getConnection();
+        if (conn == null)
+            return false;
+
+        try (PreparedStatement ps = conn.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, artista.getName());
+            ps.setString(2, artista.getPhoto());
+            ps.setString(3, artista.getNacionality());
+
+            if (ps.executeUpdate() == 1) {
+                try (ResultSet rs = ps.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        artista.setId(rs.getInt(1));
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+
+            return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean updateArtista(Artista artista) {
+        Connection conn = MariaDBConnection.getConnection();
+        if (conn == null)
+            return false;
+
+        try (PreparedStatement ps = conn.prepareStatement(UPDATE)) {
+            ps.setString(1, artista.getName());
+            ps.setString(2, artista.getPhoto());
+            ps.setString(3, artista.getNacionality());
+            ps.setInt(4, artista.getId());
+
+            return ps.executeUpdate() == 1;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean deleteArtista(Artista artista) {
+        Connection conn = MariaDBConnection.getConnection();
+        if (conn == null)
+            return false;
+
+        try (PreparedStatement ps = conn.prepareStatement(DELETE)) {
+            ps.setInt(1, artista.getId());
+
+            return ps.executeUpdate() == 1;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
     public boolean getArtista(int id) {
+        Connection conn = MariaDBConnection.getConnection();
+        if (conn == null)
+            return false;
+
+        try (PreparedStatement ps = conn.prepareStatement(SELECT_BY_ID)) {
+            ps.setInt(1, id);
+            if (ps.execute()) {
+                try (ResultSet rs = ps.getResultSet()) {
+                    return rs.next(); // Devuelve true si se encuentra un artista con el ID, de lo contrario, false.
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return false;
     }
 
     @Override
     public boolean getArtista(String name) {
+        Connection conn = MariaDBConnection.getConnection();
+        if (conn == null)
+            return false;
+
+        try (PreparedStatement ps = conn.prepareStatement(SELECT_BY_NAME)) {
+            ps.setString(1, name);
+            if (ps.execute()) {
+                try (ResultSet rs = ps.getResultSet()) {
+                    return rs.next(); // Devuelve true si se encuentra un artista con el nombre, de lo contrario, false.
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return false;
     }
 
-    @Override
+
     public List<Artista> getAllArtists() {
-        return null;
-    }
+        Connection conn = MariaDBConnection.getConnection();
+        if (conn == null)
+            return null;
 
-    @Override
-    public boolean saveArtista(Artista artista) {
-        return false;
-    }
+        List<Artista> artistas = new ArrayList<>();
 
-    @Override
-    public boolean deleteArtista(Artista artista) {
-        return false;
+        try (PreparedStatement ps = conn.prepareStatement(SELECT_ALL)) {
+            if (ps.execute()) {
+                try (ResultSet rs = ps.getResultSet()) {
+                    while (rs.next()) {
+                        Artista artista = new Artista(
+                                rs.getInt("id"),
+                                rs.getString("name"),
+                                rs.getString("photo"),
+                                rs.getString("nacionality"),
+                                null // Necesitas manejar la lista de discos apropiadamente
+                        );
+                        artistas.add(artista);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return artistas;
     }
 }
